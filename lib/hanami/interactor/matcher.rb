135820b19833ae411460fb6545b6cf081b2af545
Anton Davydov <antondavydov.o@gmail.com>
2017-11-26 13:41:46 +0300

    Create matcher module for working with interactor results

--- lib/hanami/interactor/matcher.rb
+++ lib/hanami/interactor/matcher.rb
@@ -1,9 +1,38 @@
 require "hanami/interactor/matcher/version"
+require 'hanami/interactor'
+require 'dry-matcher'
 
 module Hanami
   module Interactor
     module Matcher
       # Your code goes here...
+      #
+      SUCCESS_CASE = Dry::Matcher::Case.new(
+        match: -> result { result.successful? },
+        resolve: -> result { result.value }
+      )
+
+      FAILURE_CASE = Dry::Matcher::Case.new(
+        match: -> result { result.failure? },
+        resolve: -> result { result.value }
+      )
+
+      MatcherObject = Dry::Matcher.new(success: SUCCESS_CASE, failure: FAILURE_CASE)
+
+      def call(*args, **kwargs, &block)
+        @__result = ::Hanami::Interactor::Result.new
+        _call(*args, **kwargs) { super }
+
+        if block
+          MatcherObject.(@__result, &block)
+        else
+          @__result
+        end
+      end
     end
   end
+end
+
+module Hanami::Interactor::Interface
+  include Hanami::Interactor::Matcher
 end
